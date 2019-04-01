c63d12f06230e9540ae441d144ba427aebf53407
conanshao <conanshao@tencent.com>
2019-04-01 18:46:47 +0800

    update

--- D3D12-HookLite/D3D12CommandListHookInterface.h
+++ D3D12-HookLite/D3D12CommandListHookInterface.h
@@ -787,6 +787,13 @@ DECLARE_FUNCTIONPTR(void, D3D12SetGraphicsRootDescriptorTable, ID3D12GraphicsCom
 		for (UINT i = cbvcount; i < desccount; i++)
 		{
 			size_t newptr = desdata[i]; 
+
+			/*if (newptr == 0x23)
+			{
+				char buf[256];
+				sprintf_s(buf, "2060, there is empty bound here\n");
+				OutputDebugStringA(buf);
+			}*/
 			streamInstance->write(newptr);
 		
 		}
